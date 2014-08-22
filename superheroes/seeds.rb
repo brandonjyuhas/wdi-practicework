a46d3ad984b3bf78bd8d6d0edfe7a0908745e88e
brandonjyuhas <brandonjyuhas@gmail.com>
2014-08-22 12:02:36 -0400

    seeds

--- superheroes/seeds.rb
+++ superheroes/seeds.rb
@@ -1,8 +1,61 @@
 require_relative './hero'
 
-Heroe.create(
+Hero.create(
   [
-    { alt: "Mikael Davis", name: "Mentoring Mike", cape: false, role: "leader", img: "/images/mike.jpg"
+    { alt: "Salvador Hernandez", name: "Brooklyn Boolean", super_power: "True/False Vision", cape: true, role: "hero", img: "/images/1.jpg", weakness: "Gray Area"
+    	},
+    { alt: "Brandon Yuhas", name: "The Triple Data Distiller", super_power: "Argues with Data and returns delicious Data-Flavoured Vodka!", cape: true, role: "hero", img: "images/18.jpg", weakness: "Sobriety"
+
+    },
+        { alt: "Mandy Moore", name: "Ubuntu Ultimate", super_power: "Doesn't 'buy in' to mainstream developer computers!", cape: false, role: "hero", img: "/images/2.jpg", weakness: "Doesn't fit in at StarBucks"
+    	
+    },
+        { alt: "Paul Miller", name: "JavaScript Justicar", super_power: "Makes awesome hit counters on websites!", cape: true, role: "hero", img: "/images/3.jpg", weakness: "New CSS functions"
+    	
+    },
+        { alt: "Pam Assogba", name: "Rails Rebounder", super_power: "Scaffolds apps in record time!", cape: false, role: "hero", img: "/images/4.jpg", weakness: "Configuration over convention!"
+    	
+    },
+        { alt: "Andy Kim", name: "HTML Hero", super_power: "Super structured content!", cape: true, role: "hero", img: "/images/5.jpg", weakness: "No style whatsoever!"
+    	
+    },
+        { alt: "Ansoo Chang", name: "Giterminalator", super_power: "Never loses any data!", cape: false, role: "hero", img: "/images/6.jpg", weakness: "Merge conflicts!"
+    	
+    },
+        { alt: "Christopher Pollock", name: "Merge Master", super_power: "Doesn't have to save files after merging!", cape: false, role: "hero", img: "/images/7.jpg", weakness: "Group projects!"
+    	
+    },
+        { alt: "Clare Politano", name: "Operator Opportunist", super_power: "Strings equal to things other than 0!", cape: true, role: "hero", img: "/images/8.jpg", weakness: "Comparing Nil Class!"
+    	
+    },
+        { alt: "Daniel Kim", name: "Data Type Terror", super_power: "Super String Strength!", cape: false, role: "hero", img: "/images/9.jpg", weakness: "Doesn't know his SuperClass."
+    	
+    },
+        { alt: "Erica Geiser", name: "Righteous Rspector", super_power: "Tests everything that she's ever done!", cape: true, role: "hero", img: "/images/10.jpg", weakness: "Takes more time to do anything due to rigerous testing!"
+    	
+    },
+        { alt: "Jake Catt", name: "Sinatra Silencer", super_power: "Launches a web server without a single word!", cape: false, role: "hero", img: "/images/11.jpg", weakness: "Not knowing 'this Ditty'!"
+    	
+    },
+        { alt: "Lisa Snider", name: "Magnificent Macbook", super_power: "Very popular in Starbucks!", cape: true, role: "hero", img: "/images/12.jpg", weakness: "Cost efficiency!"
+    	
+    },
+        { alt: "Martin Johnson", name: "Courageous Commenter", super_power: "Code reads like a George R. R. Martin novel!", cape: true, role: "hero", img: "/images/13.jpg", weakness: "Still uses '//' in CSS!"
+    	
+    },
+        { alt: "Stephen Neubig", name: "Terrific TDD", super_power: "Gets awesome visual confirmation on everything!", cape: false, role: "hero", img: "/images/14.jpg", weakness: "\"undefined method '.to' \""
+    	
+    },
+        { alt: "Wade Buckland", name: "CSS Sorceror", super_power: "Styles real life objects with code!", cape: false, role: "hero", img: "/images/15.jpg", weakness: "In-line styling."
+    	
+    },
+        { alt: "Sam Barrientos", name: "Relentless Rubyist", super_power: "Writes code in plain langauge!", cape: false, role: "hero", img: "/images/16.jpg", weakness: "Compiled languages!"
+    	
+    },
+        { alt: "Liz Wendling", name: "Red Green Refractorer", super_power: "Everything eventually gets passed with one line of code!", cape: true, role: "hero", img: "/images/17.jpg", weakness: "Div soup!"
+    	
+    }
+        { alt: "Mikael Davis", name: "Mentoring Mike", cape: false, role: "leader", img: "/images/mike.jpg"
     	},
     { alt: "Sean Shannon", name: "Stuttering Sean", cape: true, role: "leader", img: "images/sean.jpg"
 
@@ -10,4 +63,5 @@ Heroe.create(
         { alt: "Jon Rojas", name: "Jolly Jon", cape: false, role: "hero", img: "/images/jon.jpg"
     	
     }
-])
+
+]
